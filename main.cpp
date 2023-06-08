d6817bce3b9062783dfde61238fe0f403cd6ec92
SayMeeveTime <zhaomo0823@gmail.com>
2023-06-08 23:40:15 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << 100 << std::endl;
+    std::cout << 'X' << std::endl;
+
+
+    std::cout << "姓名： 西施; 年龄：25; 体重:48.5; 性别: X\n";
+    std::cout << "姓名:"<<"西施"<<";年龄:"<<25<<";体重:" <<48.5;
+    std::cout << "西施";
+    std::cout << ";年龄:";
+    std::cout << 25;
+    std::cout << ";体重:";
+    std::cout << 48.5;
+    return 0;
+}
