9bd20e604fef1a41cd9e296ca0ed277eab3703ca
SayMeeveTime <zhaomo0823@gmail.com>
2023-06-09 00:42:41 +0800

    output input

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
-
+//using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << 100 << std::endl;
-    std::cout << 'X' << std::endl;
+//    std::cout << "Hello, World!" << endl;
+//    std::cout << 100 << endl;
+//    std::cout << 'X' << endl;
+//
+//
+//    std::cout << "姓名： 西施; 年龄：25; 体重:48.5; 性别: X\n";
+//    std::cout << "姓名:"<<"西施"<<";年龄:"<<25<<";体重:" <<48.5<<";性别:"<<'X'<<"\n";
+//    std::cout << "姓名:"<<"西施"<<";年龄:"<<25<<";体重:" <<48.5<<";性别:"<<'X'<<endl;
+//    std::cout << "西施";
+//    std::cout << ";年龄:";
+//    std::cout << 25;
+//    std::cout << ";体重:";
+//    std::cout << 48.5;
 
 
-    std::cout << "姓名： 西施; 年龄：25; 体重:48.5; 性别: X\n";
-    std::cout << "姓名:"<<"西施"<<";年龄:"<<25<<";体重:" <<48.5;
-    std::cout << "西施";
-    std::cout << ";年龄:";
-    std::cout << 25;
-    std::cout << ";体重:";
-    std::cout << 48.5;
-    return 0;
+    // 可以按照这个   再输入  年龄 体重  代表作
+    std::string name;
+    std::cout << "请输入女优的名称: ";
+    std::cin >> name;
+    std::cout << "已输入的女优名称: " << name << std::endl;
+
+
+
+
+//    bool  true 1   false 0
 }
